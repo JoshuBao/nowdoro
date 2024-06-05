45b362b9b4cbb26f050127bb02c4aae9c599c590
JoshuBao <Joshuaicheng@gmail.com>
2024-06-05 13:04:35 -0700

    Add autocomplete

--- src/app/error/page.tsx
+++ src/app/error/page.tsx
@@ -1,3 +1,14 @@
+'use client'
+
 export default function ErrorPage() {
-    return <p>Sorry, something went wrong</p>
-  }
+    return (
+        <div className="min-h-screen bg-base-200 flex items-center justify-center">
+            <div className="card glass lg:card-side text-neutral">
+                <div className="card-body">
+                    <h2 className="card-title text-4xl">Oops! Something went wrong</h2>
+                    <p className="text-lg mt-4">We're sorry for the inconvenience. Our team has been notified and will look into the issue.</p>
+                </div>
+            </div>
+        </div>
+    )
+}
