453b157dc18a83ee0452e960b00f381ce2ce9709
Joshua Cheng <joshuacheng888@gmail.com>
2024-06-10 15:57:59 -0700

    Stroke line fixes

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -134,19 +134,19 @@ const LandingPage: React.FC = () => {
                     <ul className="list-inside text-left text-base-content space-y-4 mt-4">
                       <li className="flex items-center">
                         <svg className="w-6 h-6 text-gray-800 dark:text-white mr-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                          <path stroke="green" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 11.917 9.724 16.5 19 7.5" />
+                          <path stroke="green" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 11.917 9.724 16.5 19 7.5" />
                         </svg>
                         Task tracking and management
                       </li>
                       <li className="flex items-center">
                         <svg className="w-6 h-6 text-gray-800 dark:text-white mr-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                          <path stroke="green" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 11.917 9.724 16.5 19 7.5" />
+                          <path stroke="green" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 11.917 9.724 16.5 19 7.5" />
                         </svg>
                         Detailed time logging for each task
                       </li>
                       <li className="flex items-center">
                         <svg className="w-6 h-6 text-gray-800 dark:text-white mr-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                          <path stroke="green" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 11.917 9.724 16.5 19 7.5" />
+                          <path stroke="green" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 11.917 9.724 16.5 19 7.5" />
                         </svg>
                         Weekly and monthly productivity reports
                       </li>
